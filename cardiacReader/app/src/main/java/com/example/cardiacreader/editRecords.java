9e6cc2adcaf5cb84d52f477d0e942ec87be6dc2f
Rafi260 <mdashrafrafi260@gmail.com>
2023-07-05 21:04:15 +0600

    .

--- cardiacReader/app/src/main/java/com/example/cardiacreader/editRecords.java
+++ cardiacReader/app/src/main/java/com/example/cardiacreader/editRecords.java
@@ -3,12 +3,24 @@ package com.example.cardiacreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class editRecords extends AppCompatActivity {
 
+    EditText etSystolic,etDiastolic,etHeartRate,etComment;
+    Button btnSave;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_records);
     }
+
+
+
 }
